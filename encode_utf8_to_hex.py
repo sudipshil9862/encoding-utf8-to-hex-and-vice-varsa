bd88e8ccda47f385afb625d8ca36764cdb4937e4
Sudip Shil <66914502+sudipshil9862@users.noreply.github.com>
2022-02-03 05:21:29 +0530

    code changes utf8 to hex and hex to utf8

--- encode_utf8_to_hex.py
+++ encode_utf8_to_hex.py
@@ -6,31 +6,31 @@ def strToHex(strValue):
         int1 = ord((ch))
         inthex = hex(int1).lstrip("0x").rstrip("L")
         #if there is any space(whose ASCII = 20) than it will covert it into "%20" (as mentioned in question)
-        if str(inthex) == "20":
-            hexa += "%20"
-        else:
-            hexa += inthex
+        # if str(inthex) == "20":
+        #     hexa += "%20"
+        # else:
+        #     hexa += inthex
+        for i in range(0,8-len(inthex)): # assumeing max 8 caharacter
+            inthex = "0" + inthex
+        hexa += inthex    
     return hexa
 
 def hexToStr(hexStr):
     asci = ""
-    for i in range(0, len(hexStr), 2):
-        part = hexStr[i : i + 2]
+    for i in range(0, len(hexStr), 8):
+        part = hexStr[i : i + 8]
         ch = chr(int(part, 16))
         asci += ch
     return asci
 
 
-
-
 s = input("enter string to convert into hex:-")
-
 #string to hex (via ASCII)
 a = strToHex(s)
 print("string to hex value:- ",a)
 
 #hex to string
-
 #removing all "%" as we have included for space instead of 20
-b = a.replace("%20","20")
-print("hex value to string:- ",hexToStr(b))
+# b = a.replace("%20","20")
+# print("hex value to string:- ",hexToStr(b))
+print("hex value to string:- ",hexToStr(a))
