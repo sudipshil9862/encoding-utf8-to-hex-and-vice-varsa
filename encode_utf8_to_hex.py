c71bc23f69c25c95f12158ac9199a3c3cd6eaaa4
Sudip Shil <66914502+sudipshil9862@users.noreply.github.com>
2022-02-15 03:58:03 +0530

    changed code utf8

--- encode_utf8_to_hex.py
+++ encode_utf8_to_hex.py
@@ -1,36 +1,34 @@
 
-def strToHex(strValue):
-    hexa = ""
-    for i  in range(len(strValue)):
-        ch = strValue[i]
-        int1 = ord((ch))
-        inthex = hex(int1).lstrip("0x").rstrip("L")
-        #if there is any space(whose ASCII = 20) than it will covert it into "%20" (as mentioned in question)
-        # if str(inthex) == "20":
-        #     hexa += "%20"
-        # else:
-        #     hexa += inthex
-        for i in range(0,8-len(inthex)): # assumeing max 8 caharacter
-            inthex = "0" + inthex
-        hexa += inthex    
-    return hexa
+def convertToHex(s):
+    utf8 = []
+    for i in s:
+        #utf8.append((str(i.encode('utf-8').hex()).lstrip("b")).replace("'","").replace("\\x",""))
+        utf8.append((str(i.encode('utf-8').hex())))
+    return utf8
 
-def hexToStr(hexStr):
-    asci = ""
-    for i in range(0, len(hexStr), 8):
-        part = hexStr[i : i + 8]
-        ch = chr(int(part, 16))
-        asci += ch
-    return asci
+def convertToString(hexStr):
+    l = []
+    for i in hexStr:
+        x = int(i,16)
+        if(x < 256):
+            res = (x).to_bytes(1, byteorder='big')
+            l.append(res.decode())
+        elif(x < pow(2,16)):
+            res = (x).to_bytes(2, byteorder='big')
+            l.append(res.decode())
+        else:
+            res = (x).to_bytes(4, byteorder='big')
+            l.append(res.decode())
+    return l
+
+s= input("enter the string : ")
+a = convertToHex(s)
+print("utf8 hex code:-")
+for i in a:
+    print(i,end=" ")
 
 
-s = input("enter string to convert into hex:-")
-#string to hex (via ASCII)
-a = strToHex(s)
-print("string to hex value:- ",a)
-
-#hex to string
-#removing all "%" as we have included for space instead of 20
-# b = a.replace("%20","20")
-# print("hex value to string:- ",hexToStr(b))
-print("hex value to string:- ",hexToStr(a))
+b = convertToString(a)
+print("\noriginal string:- ")
+for i in b:
+    print(i,end="")
